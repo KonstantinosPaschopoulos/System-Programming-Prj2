fac39ba117125c690d6c2e6708ba437a7c7f3537
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-03-25 21:19:56 +0200

    signals:

--- mirror_client.c
+++ mirror_client.c
@@ -6,21 +6,34 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <signal.h>
+
+volatile sig_atomic_t flag = 1;
+
+void catchinterrupt(int signo){
+  flag = 0;
+  printf("Caught %d\n", signo);
+}
 
 int main(int argc, char **argv){
   int i, id, b;
   DIR *common_dir, *input_dir, *mirror_dir;
   char common_path[50], input_path[50], mirror_path[50], tmp_path[100];
   FILE *logfile = NULL, *idfile = NULL;
+  static struct sigaction act;
 
-  // Checking the input from the command line
+  // Setting the signal handler
+  act.sa_handler = catchinterrupt;
+  sigfillset(&(act.sa_mask));
+  sigaction(SIGINT, &act, NULL);
+  sigaction(SIGQUIT, &act, NULL);
+
+  // Parsing the input from the command line
   if (argc != 13)
   {
     printf("Correct use is: ./mirror_client -n id -c common_dir -i input_dir -m mirror_dir -b buffer_size -l log_file\n");
     exit(1);
   }
-
-  // Parsing the input
   for (i = 1; i < argc; i++)
   {
     if (strcmp(argv[i], "-n") == 0)
@@ -139,7 +152,6 @@ int main(int argc, char **argv){
     }
   }
 
-  // Creating the .id file in the common_dir
   sprintf(tmp_path, "%s/%d.id", common_path, id);
 
   // Checking if the .id file exists already and is in use by a different client
@@ -149,11 +161,11 @@ int main(int argc, char **argv){
     exit(1);
   }
 
-  // Creating the .id file and writing the pid on it
+  // Creating the .id file in the common_dir and writing the pid on it
   idfile = fopen(tmp_path, "w");
   if (idfile == NULL)
   {
-    perror("Couldn't open idfile");
+    perror("Couldn't open the .id file");
     exit(2);
   }
   fprintf(idfile, "%d", getpid());
