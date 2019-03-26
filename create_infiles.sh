d3ee6e1bc4b9dc5b5b51541f86bd7afed7710579
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-03-26 20:42:56 +0200

    fixed quotes

--- create_infiles.sh
+++ create_infiles.sh
@@ -2,7 +2,7 @@
 declare -a dir_names
 
 # Checks the number of arguments
-if [ "$#" -ne 4 ]; then
+if [ $# -ne 4 ]; then
 	echo Correct use: ./create_infiles.sh dir_name num_of_files num_of_dirs levels
 	exit 1
 fi
@@ -11,15 +11,15 @@ fi
 if [ -e "$1" ]; then
 	# Checks if the first argument is a directory
 	if ! [ -d "$1" ]; then
-		echo $1 exists, but it is not a directory
+		echo "$1" exists, but it is not a directory
 		exit 1
 	fi
 else
-	mkdir -p $1
+	mkdir -p "$1"
 fi
 
 # Creating the directory names
-for ((i=0; i < "$3"; i++)); do
+for ((i=0; i < $3; i++)); do
 	# Deciding how long the name of each directory will be using shuf
 	length=`shuf -i 1-8 -n 1`
 	tmp=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c $length`
@@ -36,8 +36,8 @@ while [ $i -lt $3 ]; do
 	j=1
 	# The internal loop counts how many levels have been created
 	while [ $j -lt $4 ]; do
-		if [ $(( j+i )) -gt "$3" ]; then
-			exit
+		if [ $(( j+i )) -gt $3 ]; then
+			exit 0
 		fi
 		path="${path}/${dir_names[i+j]}"
 		mkdir -p "$path"
